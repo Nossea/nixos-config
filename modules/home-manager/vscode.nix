1ef979ce6bf1c8479cfc84d5a4fd8f4048798fc5
Nossea <iamnossea@gmail.com>
2024-03-11 13:04:18 -0700

    First Commit Test

---
+++ modules/home-manager/vscode.nix
@@ -0,0 +1,12 @@
+{ inputs, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+    ];
+  };
+}
---
+++ modules/outdoors/steam.nix
@@ -0,0 +1,9 @@
+{ inputs, pkgs, ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+}
