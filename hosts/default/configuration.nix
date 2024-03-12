5603ccdea29921779573bcf21dabedc2a176b105
Nossea <iamnossea@gmail.com>
2024-03-12 12:53:44 -0700

    2024-03-12 12:53:28

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -75,15 +75,16 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
+  services.xserver.desktopManager = {
+    plasma5.enable = true;
+    xterm.enable = false;
+  };
   # Exclude unwanted packages from Plasma.
   environment.plasma5.excludePackages = with pkgs.libsForQt5; [
     plasma-browser-integration
     konsole
     oxygen
   ];
-
 
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
