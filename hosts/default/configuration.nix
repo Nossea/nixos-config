b16b989c6169a8712bc3707d9de27aec4f2f1d0b
Nossea <iamnossea@gmail.com>
2024-03-12 15:02:36 -0700

    2024-03-12 15:02:13

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -7,16 +7,6 @@
 {
   #  ------- SYSTEM SETTINGS -------  #
 
-
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
-
-
-  # Enable flakes:
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Enable flake-based autoupdating.
   system.autoUpgrade = {
     enable = true;
@@ -39,47 +29,14 @@
   networking.hostName = "nixos"; 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
 
   # Enable the X11 windowing system.
@@ -100,11 +57,6 @@
     plasma-browser-integration
     konsole
     oxygen
-  ];
-
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    # Add any missing dynamic libraries for unpackaged programs here, NOT in environment.systemPackages
   ];
 
 
@@ -147,6 +99,7 @@
 
   imports =
     [ # Include the results of the hardware scan.
+      ../../modules/nixos/global.nix
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
       ../../modules/outdoors/steam.nix
---
+++ modules/nixos/global.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, inputs, ... }:
+
+{
+    # All global packages are here. Basically a "starter config".
+
+    # Bootloader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/vda";
+    boot.loader.grub.useOSProber = true;
+
+
+    # Enable flakes:
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
+
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+        # Add any missing dynamic libraries for unpackaged programs here, NOT in environment.systemPackages
+    ];
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+}
