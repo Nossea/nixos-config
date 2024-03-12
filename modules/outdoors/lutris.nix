68677fbaa3002c50d3f7ae1ce4a16762910870e4
Nossea <iamnossea@gmail.com>
2024-03-12 11:19:32 -0700

    2024-03-12 11:18:58

---
+++ modules/outdoors/lutris.nix
@@ -0,0 +1,21 @@
+{config, pkgs, ...}:
+
+{
+    environment.systemPackages = with pkgs; [
+        # Lutris
+        #lutris-unwrapped  # (not needed)
+        lutris
+
+        # Extra dependencies
+        # https://github.com/lutris/docs/
+        gnutls
+        openldap
+        libgpgerror
+        freetype
+        sqlite
+        libxml2
+        xml2
+        SDL2
+    ];
+}
+    
--- modules/outdoors/steam.nix
+++ modules/outdoors/steam.nix
@@ -22,20 +22,5 @@
     winetricks
     protontricks
     vulkan-tools
-
-    # Lutris
-    #lutris-unwrapped  # (not needed)
-    lutris
-
-    # Extra dependencies
-    # https://github.com/lutris/docs/
-    gnutls
-    openldap
-    libgpgerror
-    freetype
-    sqlite
-    libxml2
-    xml2
-    SDL2
   ];
 }
