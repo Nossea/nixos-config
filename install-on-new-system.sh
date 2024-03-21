c93a28335187185fbc5b720d6018a10985aa8e89
Nossea <iamnossea@gmail.com>
2024-03-20 20:27:04 -0700

    Version 39: 2024-03-20 20:26:48

---
+++ install-on-new-system.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "
+!!CAREFUL!! This will overwrite ALL "hardware-configuration.nix" files within the hosts directory.
+This is good if you are installing on a new system but bad if you edited them.
+Are you sure you want to do this?
+"
+
+sudo nixos-generate-config --show-hardware-config > ./hardware-configuration.nix
+find ./hosts/* -type d -exec cp ./hardware-configuration.nix {} \;
+rm ./hardware-configuration.nix
+
+sudo nixos-rebuild switch --flake /home/${USER}/nixos-config#default &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+
+echo "System rebuilt successfully! Don't forget to edit the nixos-rebuild.sh script if you want to use git!
+PS: This is a great tutorial to set that up: https://www.youtube.com/watch?v=jTHhMSxQTNI"
