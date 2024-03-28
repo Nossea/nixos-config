261216d33a550dc09ad801124bea54a3928df988
Nossea <iamnossea@gmail.com>
2024-03-28 01:23:25 -0700

    Version 76: 2024-03-28 01:22:44

--- modules/program-files/dependancies/lib.nix
+++ modules/program-files/dependancies/lib.nix
@@ -6,29 +6,29 @@
         # Add any missing dynamic libraries for unpackaged programs here, NOT in environment.systemPackages
 
         # DNA
-        #glib
-        #nss
-        #nspr
-        #dbus
-        #atk
-        #cups
-        #libdrm
-        #gtk3
-        #pango
-        #cairo
-        #xorg.libX11
-        #xorg.libXcomposite
-        #xorg.libXdamage
-        #xorg.libXext
-        #xorg.libXfixes
-        #xorg.libXrandr
-        #xorg.libxcb
-        #libxkbcommon
-        #mesa
-        #expat
-        #alsa-lib
-        #electron
-        #libglvnd
+        glib
+        nss
+        nspr
+        dbus
+        atk
+        cups
+        libdrm
+        gtk3
+        pango
+        cairo
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+        xorg.libxcb
+        libxkbcommon
+        mesa
+        expat
+        alsa-lib
+        electron
+        libglvnd
         # End of DNA (or just use steam-run like a normal person (since this doesn't actually work anyways :P))
     ];
 }
