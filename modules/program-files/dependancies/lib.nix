3b2fbbce6394612280a5dfe86d509648ef96a55e
Nossea <iamnossea@gmail.com>
2024-04-21 01:19:08 -0700

    Version 89: 2024-04-21 01:18:35

--- modules/program-files/dependancies/lib.nix
+++ modules/program-files/dependancies/lib.nix
@@ -5,5 +5,5 @@
         ## If needed, you can add missing libraries here. nix-index-database is your friend to
         ## find the name of the package from the error message:
         ## https://github.com/nix-community/nix-index-database
-    #programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [ yourlibrary ]);
+    #programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [ python311Packages.pyside6 ]);
 }
