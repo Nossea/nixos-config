9d67f435dbd37529ccd9d3eed2c81ef562000f13
Nossea <iamnossea@gmail.com>
2024-06-10 10:59:22 -0700

    Version 155: 2024-06-10 10:58:54

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -3,13 +3,15 @@
 {
     programs.kitty = {
         enable = true;
-        theme = "Adventure Time";
+        theme = "Catppuccin-Mocha";
         font = {
             package = pkgs.comic-mono;
             name = "Comic Mono";
         };
+
         settings = {
-            confirm_os_window_close = -0;
+            confirm_os_window_close = 0;
+            background_opacity = "0.5";
         };
     };
 }
