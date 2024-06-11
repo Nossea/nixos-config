1ffa946ed8236c51538345869b5d12e57ea61757
Nossea <iamnossea@gmail.com>
2024-06-10 23:48:50 -0700

    Version 180: 2024-06-10 23:47:06

--- modules/home-manager/hyprhome.nix
+++ modules/home-manager/hyprhome.nix
@@ -277,5 +277,28 @@
 
         };
     };
+
+    programs.waybar = {
+        enable = true;
+
+        settings = {
+            mainBar = {
+                layer = "top";
+                modules-left = ["sway/workspaces" "sway/mode"];
+                modules-center = ["sway/window"];
+                modules-right = ["battery" "clock"];
+                "sway/window" = {
+                    max-length = 50;
+                };
+                battery = {
+                    format = "{capacity}% {icon}";
+                    format-icons = ["" "" "" "" ""];
+                };
+                clock = {
+                    format-alt = "{:%a, %d. %b  %H:%M}";
+                };
+            };
+        };
+    };
 }
  
--- modules/program-files/global.nix
+++ modules/program-files/global.nix
@@ -16,7 +16,7 @@
     # Global packages
     environment.systemPackages = with pkgs; 
     [
-        libnotify
+        swaynotificationcenter
         firefox
         pika-backup
         libsForQt5.ark
