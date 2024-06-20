b1ba338ee76567b036467963bd908804418ae4e8
Nossea <iamnossea@gmail.com>
2024-06-20 15:06:52 -0700

    Version 204: 2024-06-20 15:06:17

--- modules/home-manager/discord.nix
+++ modules/home-manager/discord.nix
@@ -2,7 +2,7 @@
 
 {
     home.packages = with pkgs; [
-        (pkgs.discord.override { withVencord = true; })
-        vesktop
+        #(pkgs.discord.override { withVencord = true; })
+        #vesktop
     ];
 }
